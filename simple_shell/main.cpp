6ee53a5d9423205195866224e8b68f94f62d7be3
Svetlana Rokashevich <sv_ch@live.com>
2021-01-04 21:46:58 +0300

    вывод из stdint + fix Makefile

--- simple_shell/main.cpp
+++ simple_shell/main.cpp
@@ -1,1 +1,9 @@
-int main() { return 0; }
+#include <iostream>
+#include <string>
+
+int main() {
+  for (std::string line; std::getline(std::cin, line);) {
+    std::cout << line << std::endl;
+  }
+  return 0;
+}
